f07f0a2b230b238a69e70178d907424f1c6d55a1
Juan Sebastian Castañeda Burbano <js.castanedaburbano@gmail.com>
2024-09-07 21:57:36 -0500

    create array of all posible window naves

--- app/main.py
+++ app/main.py
@@ -1,1 +1,8 @@
-print("Hello world")
+from app.classes.window_naves import WindowNaves
+
+WINDOWS_NAVES: list[WindowNaves] = [
+    WindowNaves("O"),
+    WindowNaves("XO"),
+    WindowNaves("OXO"),
+    WindowNaves("OXXO"),
+]
